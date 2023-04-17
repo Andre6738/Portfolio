77791dd08b901183f8e3fb28d3de634d0d3c3c39
Andre Croukamp <andre.croukamp@entelect.co.za>
2023-04-17 08:30:25 +0200

    animations for headings

--- portfolio-react/src/Home/Home.js
+++ portfolio-react/src/Home/Home.js
@@ -5,10 +5,13 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import emailjs from 'emailjs-com';
 import Swal from 'sweetalert2';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import anime from 'animejs/lib/anime.es.js';
 
 function Home() {
   const [sending, setSending] = useState(false);
+  const [zoom, setZoom] = useState(7);
+  
   const defaultIcon = L.icon({
     iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
     iconSize: [25, 41],
@@ -49,14 +52,61 @@ function Home() {
 
   return (
     <div className="main-wrapper">
-      <header>
-
+      <header className='header'>
+        <div className='top-heading-container'>
+          <span className='Before h1'></span>
+          <h1><span>H</span><span>i</span><span>,</span></h1>
+          <h1>
+              <span>I</span>
+              <span>'</span>
+              <span>m</span>
+              <span className='invisable'>m</span>
+              <span>A</span>
+              <span>n</span>
+              <span>d</span>
+              <span>r</span>
+              <span>é</span>
+              <span>,</span>
+          </h1>
+          <h1>
+              <span>S</span>
+              <span>o</span>
+              <span>f</span>
+              <span>t</span>
+              <span>w</span>
+              <span>a</span>
+              <span>r</span>
+              <span>e</span>
+              <span className='invisable'>m</span>
+              <span>E</span>
+              <span>n</span>
+              <span>g</span>
+              <span>i</span>
+              <span>n</span>
+              <span>e</span>
+              <span>e</span>
+              <span>r</span>
+          </h1>
+          <span className='Before h1-after'></span>
+          <div className='div-button-top'>
+            <button id='email-button' onClick={() => {
+              const footer = document.querySelector('footer');
+              window.scrollTo({
+                  top: footer.offsetTop,
+                  behavior: 'smooth'
+              });
+              }} >Contact me!</button>
+          </div>
+        </div>
       </header>
       <div className="middle">
 
+
+
       </div>
       <footer>
-        <div>
+        <div className='footer'>
+        <div className='div-back-color'>
           <div className='contact-center'>
             <div>
             <span className='Before h2'></span>
@@ -94,25 +144,32 @@ function Home() {
           <span className='After body-after'></span>
           <span className='After html-after'></span>
         </div>
-        <MapContainer
-          center={[-25.881040, 28.203550]}
-          zoom={7}
-          scrollWheelZoom={false}
-          style={{ height: "100%", width: "100%" }}
-        >
-          <TileLayer
-            url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-            attribution='&copy; <a href="https://goo.gl/maps/qRhHMPpLMkQBz9qZ8">OpenGoogleMaps</a>'
-          />
-          <Marker position={[-25.881040, 28.203550]} icon={defaultIcon}>
-            <Popup>
-            Entelect Centurion Office
-            </Popup>
-          </Marker>
-        </MapContainer>
+    <MapContainer
+      key={zoom}
+      center={[-25.881040, 28.203550]}
+      zoom={zoom}
+      scrollWheelZoom={false}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <TileLayer
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://goo.gl/maps/qRhHMPpLMkQBz9qZ8">OpenGoogleMaps</a>'
+      />
+      <Marker position={[-25.881040, 28.203550]} icon={defaultIcon}>
+        <Popup>Entelect Centurion Office</Popup>
+      </Marker>
+    </MapContainer>
+        </div>
+        <div className='footer-top'>
+          <div className='to-the-top' onClick={()=>window.scrollTo({ top: 0, behavior: 'smooth' })}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="1.5vw" height="1.5vw" fill="currentColor" class="bi bi-chevron-double-up bounce-icon" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708l6-6z" style={{ fill: "#9CDCFE" }} />
+            <path fill-rule="evenodd" d="M7.646 6.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 7.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z" style={{ fill: "#9CDCFE", opacity:'0.6' }} />
+          </svg>
+          </div>
+        </div>
       </footer>
     </div>
   );
 }
-
 export default Home;
